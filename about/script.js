7733b78a34bef11ad54972181de90e7947c8315f
pixy <argosdash@gmail.com>
2024-12-16 21:40:29 -0500

    fir

---
+++ about/script.js
@@ -0,0 +1,18 @@
+document.getElementById("animateBtn").addEventListener("click", () => {
+    const img1 = document.getElementById("img1");
+    const img2 = document.getElementById("img2");
+    const img3 = document.getElementById("img3");
+    const img4 = document.getElementById("img4");
+    const img5 = document.getElementById("img5");
+  
+    // Move images off-frame
+    img1.style.transform = "translate(400%, 00%)";
+    img2.style.transform = "translate(0%, 300%)";
+    img3.style.transform = "translate(-400%, 00%)";
+    img4.style.transform = "translate(00%, 300%)";
+  
+    // Zoom in and fade out the final image
+    img5.style.transform = "scale(3)";
+    img5.style.opacity = "0";
+  });
+  
